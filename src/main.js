cab9b1a203c3b9115c6b6c2e5dbaead8521aad75
Lei <caol64@gmail.com>
2024-09-14 15:22:14 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,129 @@
+// import { resolveResource } from '@tauri-apps/api/path'
+const { resolveResource } = window.__TAURI__.path;
+// import { readTextFile } from '@tauri-apps/api/fs'
+const { readTextFile } = window.__TAURI__.fs;
+// import { appWindow } from '@tauri-apps/api/window'
+const { appWindow } = window.__TAURI__.window;
+
+// const { invoke } = window.__TAURI__.tauri;
+
+// let greetInputEl;
+// let greetMsgEl;
+let theme = "themes/gzh_default.css";
+let highlightStyle = "highlight/styles/github.min.css";
+let previewMode = "style.css";
+let content = "";
+let isCopied = false
+let isFootnotes = false
+
+// async function greet() {
+//   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+//   greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+// }
+
+// window.addEventListener("DOMContentLoaded", async () => {
+//   try {
+//     const resourcePath = await resolveResource('resources/example.md');
+//     content = await readTextFile(resourcePath);
+//     onUpdate();
+//   } catch (error) {
+//     console.error("Error reading file:", error);
+//   }
+// });
+
+document
+  .getElementById('titlebar-minimize')
+  .addEventListener('click', () => appWindow.minimize())
+document
+  .getElementById('titlebar-maximize')
+  .addEventListener('click', () => appWindow.toggleMaximize())
+document
+  .getElementById('titlebar-close')
+  .addEventListener('click', () => appWindow.close())
+
+window.addEventListener('message', async (event) => {
+  if (event.data) {
+    if (event.data.type === "onReady") {
+      try {
+        const resourcePath = await resolveResource('resources/example.md');
+        content = await readTextFile(resourcePath);
+        const iframe = document.getElementById('leftFrame');
+        if (iframe) {
+          const message = {
+            type: 'onUpdate',
+            value: content
+          };
+          iframe.contentWindow.postMessage(message, '*');
+        }
+        onUpdate();
+      } catch (error) {
+        console.error("Error reading file:", error);
+      }
+    } else if (event.data.type === "onChange") {
+      content = event.data.value;
+      onUpdate();
+    }
+  }
+});
+
+function onUpdate() {
+  const iframe = document.getElementById('rightFrame');
+  if (iframe) {
+    const message = {
+      type: 'onUpdate',
+      content: content,
+      theme: theme,
+      highlightStyle: highlightStyle,
+      previewMode: previewMode,
+    };
+    iframe.contentWindow.postMessage(message, '*');
+  }
+}
+
+function onContentChange() {
+  const iframe = document.getElementById('rightFrame');
+  if (iframe) {
+    const message = {
+      type: 'onContentChange',
+      content: content,
+    };
+    iframe.contentWindow.postMessage(message, '*');
+  }
+}
+
+function onPeviewModeChange(button) {
+  const useElement = button.querySelector('use');
+  if (previewMode === "style.css") {
+    previewMode = "desktop_style.css";
+    useElement.setAttribute('href', '#mobileIcon');
+  } else {
+    previewMode = "style.css";
+    useElement.setAttribute('href', '#desktopIcon');
+  }
+  const iframe = document.getElementById('rightFrame');
+  if (iframe) {
+    const message = {
+      type: 'onPeviewModeChange',
+      previewMode: previewMode,
+    };
+    iframe.contentWindow.postMessage(message, '*');
+  }
+}
+
+function onFootnoteChange(button) {
+  isFootnotes = !isFootnotes;
+  const useElement = button.querySelector('use');
+  if (isFootnotes) {
+    useElement.setAttribute('href', '#footnoteIcon');
+    const iframe = document.getElementById('rightFrame');
+    if (iframe) {
+      const message = {
+        type: 'onFootnoteChange'
+      };
+      iframe.contentWindow.postMessage(message, '*');
+    }
+  } else {
+    useElement.setAttribute('href', '#footnoteIcon');
+    onContentChange();
+  }
+}
---
+++ src/right.js
@@ -0,0 +1,153 @@
+const { markedHighlight } = globalThis.markedHighlight;
+
+let postprocessMarkdown = "";
+let isScrollingFromScript = false;
+
+
+function preprocess(markdown) {
+    const { attributes, body } = window.frontMatter(markdown);
+    let head = "";
+    if (attributes['title']) {
+        head = "# " + attributes['title'] + "\n\n";
+    }
+    if (attributes['description']) {
+        head += "> " + attributes['description'] + "\n\n";
+    }
+    postprocessMarkdown = head + body;
+    return postprocessMarkdown;
+}
+marked.use({ hooks: { preprocess } });
+marked.use(markedHighlight({
+    langPrefix: "hljs language-",
+    highlight: function (code, language) {
+        language = hljs.getLanguage(language) ? language : "plaintext";
+        return hljs.highlight(code, { language: language }).value;
+    }
+}));
+function setStylesheet(id, href) {
+    const style = document.createElement("link");
+    style.setAttribute("id", id);
+    style.setAttribute("rel", "stylesheet");
+    style.setAttribute("href", href);
+    document.head.appendChild(style);
+}
+function setContent(content) {
+    document.getElementById("wenyan")?.remove();
+    const container = document.createElement("section");
+    container.innerHTML = marked.parse(content);
+    container.setAttribute("id", "wenyan");
+    container.setAttribute("class", "preview");
+    document.body.appendChild(container);
+    MathJax.typeset();
+}
+function setPreviewMode(mode) {
+    document.getElementById("style")?.remove();
+    setStylesheet("style", mode)
+}
+function setTheme(theme) {
+    document.getElementById("theme")?.remove();
+    setStylesheet("theme", theme)
+}
+function setHighlight(highlight) {
+    document.getElementById("hljs")?.remove();
+    if (highlight) {
+        setStylesheet("hljs", highlight);
+    }
+}
+function getContent() {
+    const wenyan = document.getElementById("wenyan");
+    const clonedWenyan = wenyan.cloneNode(true);
+    const elements = clonedWenyan.querySelectorAll("mjx-container");
+    elements.forEach(element => {
+        const math = element.getAttribute("math");
+        const parent = element.parentElement;
+        element.remove();
+        parent.innerHTML = math;
+    });
+    return clonedWenyan.outerHTML;
+}
+function getContentWithMathImg() {
+    const wenyan = document.getElementById("wenyan");
+    const clonedWenyan = wenyan.cloneNode(true);
+    const elements = clonedWenyan.querySelectorAll("mjx-container");
+    elements.forEach(element => {
+        const math = element.getAttribute("math");
+        const parent = element.parentElement;
+        element.remove();
+        let img = document.createElement("img");
+        img.setAttribute("alt", math);
+        img.setAttribute("data-eeimg", "true");
+        img.setAttribute("style", "margin: 0 auto; width: auto; max-width: 100%;");
+        parent.appendChild(img);
+    });
+    return clonedWenyan.outerHTML;
+}
+function getContentWithMathSvg() {
+    const wenyan = document.getElementById("wenyan");
+    const clonedWenyan = wenyan.cloneNode(true);
+    const elements = clonedWenyan.querySelectorAll("mjx-container");
+    elements.forEach(element => {
+        const svg = element.querySelector('svg');
+        const parent = element.parentElement;
+        element.remove();
+        parent.appendChild(svg);
+    });
+    return clonedWenyan.outerHTML;
+}
+function getPostprocessMarkdown() {
+    return postprocessMarkdown;
+}
+function scroll(scrollFactor) {
+    isScrollingFromScript = true;
+    window.scrollTo(0, document.body.scrollHeight * scrollFactor);
+    requestAnimationFrame(() => isScrollingFromScript = false);
+}
+window.onscroll = function () {
+    if (!isScrollingFromScript) {
+        // window.webkit.messageHandlers.scrollHandler.postMessage({ y0: window.scrollY / document.body.scrollHeight });
+    }
+};
+function addFootnotes() {
+    let footnotes = [];
+    let footnoteIndex = 0;
+    const links = document.querySelectorAll('a[href]'); // 获取所有带有 href 的 a 元素
+    links.forEach((linkElement) => {
+        const title = linkElement.textContent || linkElement.innerText;
+        const href = linkElement.getAttribute('href');
+
+        // 添加脚注并获取脚注编号
+        footnotes.push([++footnoteIndex, title, href]);
+
+        // 在链接后插入脚注标记
+        const footnoteMarker = document.createElement('sup');
+        footnoteMarker.innerHTML = `[${footnoteIndex}]`;
+        linkElement.after(footnoteMarker);
+    });
+    if (footnoteIndex > 0) {
+        let footnoteArray = footnotes.map((x) => {
+            if (x[1] === x[2]) {
+                return `<li id="#footnote-${x[0]}">[${x[0]}]: <i>${x[1]}</i></li>`;
+            }
+            return `<li id="#footnote-${x[0]}">[${x[0]}] ${x[1]}: <i>${x[2]}</i></li>`;
+        });
+        const footnotesHtml = `<h3>引用链接</h3><div id="footnotes"><ul>${footnoteArray.join("")}</ul></div>`;
+        document.getElementById("wenyan").innerHTML += footnotesHtml;
+    }
+}
+
+window.addEventListener('message', (event) => {
+    if (event.data) {
+        if (event.data.type === 'onUpdate') {
+            setContent(event.data.content);
+            setTheme(event.data.theme);
+            setHighlight(event.data.highlightStyle);
+            setPreviewMode(event.data.previewMode);
+        } else if (event.data.type === 'onContentChange') {
+            setContent(event.data.content);
+        } else if (event.data.type === 'onPeviewModeChange') {
+            setPreviewMode(event.data.previewMode);
+        } else if (event.data.type === 'onFootnoteChange') {
+            addFootnotes();
+        }
+    }
+});
