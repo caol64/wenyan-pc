5b044e41ad2c645655defcc1be48ba48813ed493
caol64 <caol64@gmail.com>
2025-02-22 15:59:19 +0800

    修复圆角边框

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,7 +2,6 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use tauri::Manager;
-use window_vibrancy::{apply_blur, apply_vibrancy, NSVisualEffectMaterial};
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -21,13 +20,7 @@ fn main() {
     tauri::Builder::default()
         .setup(|app| {
             let main_window = app.get_window("main").unwrap();
-            #[cfg(target_os = "macos")]
-            apply_vibrancy(&main_window, NSVisualEffectMaterial::HudWindow, None, Some(10.0))
-                .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
 
-            #[cfg(target_os = "windows")]
-            apply_blur(&main_window, Some((18, 18, 18, 0)))
-                .expect("Unsupported platform! 'apply_blur' is only supported on Windows");
             // 获取线程安全的 AppHandle
             let app_handle = app.handle();
 
