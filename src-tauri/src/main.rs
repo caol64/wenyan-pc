ef0840548607624704b0d56dfe48798d97d9999c
Pang <pangyuyu@illusiontech.cn>
2025-03-03 12:00:32 +0800

    feat: tauri v1 to v2,剩余一个icon问题待解决

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,36 +1,38 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::Manager;
+use tauri::{Listener, Manager, WebviewUrl};
+use tauri_plugin_clipboard_manager::ClipboardExt;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn write_html_to_clipboard(app: tauri::AppHandle, text: String) {
-    let clipboard = app.state::<tauri_plugin_clipboard::ClipboardManager>();
-    clipboard.write_html(text).unwrap();
+    app.clipboard()
+        .write_html(text, Some("<p>Html 写入剪切板失败</p>".to_string()))
+        .unwrap();
 }
 
 #[tauri::command]
 fn write_text_to_clipboard(app: tauri::AppHandle, text: String) {
-    let clipboard = app.state::<tauri_plugin_clipboard::ClipboardManager>();
-    clipboard.write_text(text).unwrap();
+    app.clipboard().write_text(text).unwrap();
 }
 
 fn main() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_clipboard_manager::init())
         .setup(|app| {
-            let main_window = app.get_window("main").unwrap();
+            let main_window = app.get_webview_window("main").unwrap();
 
             // 获取线程安全的 AppHandle
-            let app_handle = app.handle();
-
+            let app_handle = app.handle().clone();
             main_window.listen("open-about", move |_| {
                 // 检查是否已经存在 ID 为 "about" 的窗口
-                if app_handle.get_window("about").is_none() {
-                    tauri::WindowBuilder::new(
-                        &app_handle, // 使用线程安全的 app_handle
-                        "about",     // 新窗口的 id
-                        tauri::WindowUrl::App("about.html".into()) // 指定关于页面的路径
+                if app_handle.get_webview_window("about").is_none() {
+                    // 创建新窗口
+                    tauri::WebviewWindowBuilder::new(
+                        &app_handle, // 使用线程安全的 app_handle,
+                        "about",
+                        WebviewUrl::App("about.html".into()),
                     )
                     .title("关于")
                     .inner_size(350.0, 200.0)
@@ -47,7 +49,10 @@ fn main() {
         .plugin(tauri_plugin_clipboard::init())
         .plugin(tauri_plugin_sql::Builder::default().build())
         // .plugin(tauri_plugin_store::Builder::default().build())
-        .invoke_handler(tauri::generate_handler![write_html_to_clipboard, write_text_to_clipboard])
+        .invoke_handler(tauri::generate_handler![
+            write_html_to_clipboard,
+            write_text_to_clipboard
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
